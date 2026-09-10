a5775cf7020f12c662b37ff3ebf067e1fba72ec6
kim jiwon <wldnjsdl7452@gmail.com>
2026-09-10 15:42:20 +0900

    [D3] Title: 햄버거 다이어트, Time: 170 ms, Memory: 30,208 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.*;
 
 class Solution{
-	static int N;
-	static int L;
-	static int[] score;
-	static int[] kcal;
-	static int max;
+	static int N, L, max;
+	static int[] score, kcal;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,32 +10,32 @@ class Solution{
 		
 		for (int tc=1; tc<=test_case; tc++) {
 			N = sc.nextInt();
-			L = sc.nextInt();
-			score = new int[N];
-			kcal = new int[N]; 
+			L= sc.nextInt();
 			max = 0;
 			
+			score = new int[N];
+			kcal = new int[N];
+			
 			for (int i=0; i<N; i++) {
-				score[i] = sc.nextInt();	
+				score[i]= sc.nextInt();
 				kcal[i] = sc.nextInt();
 			}
 			
 			maxBurger(0,0,0);
 			System.out.printf("#%d %d%n", tc, max);
 			
-		}	//tc
+		}	// tc
 	}	//main
 	
 	static void maxBurger(int idx, int currentScore, int currentKcal) {
-		if (currentKcal > L) return;
+		if (currentKcal> L) return;
 		if (idx >= N) {
 			max = Math.max(max, currentScore);
 			return;
 		}
 		
-		maxBurger(idx+1, currentScore+score[idx], currentKcal+kcal[idx]);
+		maxBurger(idx+1, currentScore+score[idx], currentKcal + kcal[idx]);
 		maxBurger(idx+1, currentScore, currentKcal);
-		
-	} // maxBurger
+	}
 	
 }
